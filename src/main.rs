a302442a05e6f89c82c4c4ba57ab3cc7ee829fd1
Henrique Pousa Caltram <henrivai@hotmail.com>
2024-01-31 08:39:33 -0300

    Add random number generation using rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
@@ -6,8 +7,12 @@ fn main() {
     println!("Please, input a number of your choice:");
 
     let mut number_input: String = String::new();
+    let random_number: u32 = rand::thread_rng().gen_range(1..=100);
     let stdin_interface: io::Stdin = io::stdin();
 
     stdin_interface.read_line(&mut number_input)
         .expect("Failed to read input value");
+
+    println!("You guessed {number_input}");
+    println!("The correct number was {random_number}");
 }
