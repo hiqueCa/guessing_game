cca10dad96bd6c10370ab3d6f8178d3d94d031af
Henrique Pousa Caltram <henrivai@hotmail.com>
2024-01-31 08:25:04 -0300

    Implement user number input functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please, input a number of your choice:");
+
+    let mut number_input: String = String::new();
+    let stdin_interface: io::Stdin = io::stdin();
+
+    stdin_interface.read_line(&mut number_input)
+        .expect("Failed to read input value");
 }
