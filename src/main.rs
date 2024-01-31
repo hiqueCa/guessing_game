6a20c6d92388fbdf82c3698fdf20e641cc770363
Henrique Pousa Caltram <henrivai@hotmail.com>
2024-01-31 09:02:25 -0300

    Implement pattern matching to properly check user input result

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::{io, u32};
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -13,6 +14,21 @@ fn main() {
     stdin_interface.read_line(&mut number_input)
         .expect("Failed to read input value");
 
+    let number_input: u32 = number_input
+        .trim()
+        .parse()
+        .expect("Type a valid number.");
+
     println!("You guessed {number_input}");
     println!("The correct number was {random_number}");
+
+    compare_numbers(number_input, random_number);
+}
+
+fn compare_numbers(guessed_number: u32, actual_number: u32) {
+    match guessed_number.cmp(&actual_number) {
+        Ordering::Equal => println!("You guessed the right number!"),
+        Ordering::Greater => println!("You guessed a greater number."),
+        Ordering::Less => println!("You guessed a smaller number."),
+    };
 }
